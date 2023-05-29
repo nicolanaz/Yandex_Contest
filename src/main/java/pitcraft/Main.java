beda89d8d6cefb5ec8fd230e3ba5cc1bde4c051b
nicolanaz <nicola.dzr@gmail.com>
2023-05-29 11:24:23 +0300

    PitCraft

--- src/main/java/pitcraft/Main.java
+++ src/main/java/pitcraft/Main.java
@@ -1,31 +1,45 @@
 package pitcraft;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        Scanner scanner = new Scanner(System.in);
 
-            int n = Integer.parseInt(reader.readLine());
-            int[] mountains = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = scanner.nextInt();
+        int[] heights = new int[n];
 
-            int result = 0;
-            int max = mountains[0];
-
-            for (int i = 1; i < n; i++) {
-                if (mountains[i] < max) {
-                    result += max - mountains[i];
-                } else {
-                    max = mountains[i];
-                }
-            }
-
-            System.out.println(result);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (int i = 0; i < n; i++) {
+            heights[i] = scanner.nextInt();
         }
+
+        int waterBlocks = calculateWaterBlocks(heights);
+
+        System.out.println(waterBlocks);
     }
+
+    private static int calculateWaterBlocks(int[] heights) {
+        int n = heights.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = heights[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], heights[i]);
+        }
+
+        rightMax[n - 1] = heights[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], heights[i]);
+        }
+
+        int waterBlocks = 0;
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            waterBlocks += Math.max(0, waterLevel - heights[i]);
+        }
+
+        return waterBlocks;
+    }
+
 }
